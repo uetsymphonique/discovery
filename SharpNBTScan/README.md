5090d90948fef74dd6586f5a69831e83dd7ac9ed
uetsymphonique <minhtran6464.lhp@gmail.com>
2026-08-17 16:56:17 +0700

    sharpnbtscan

---
+++ SharpNBTScan/README.md
@@ -0,0 +1,14 @@
+# SharpNBTScan
+NetBIOS scanning tool written in C#
+## Usage
+```
+Usage:
+    .\SharpNBTScan.exe 192.168.0.1/24
+```
+## Example
+Retrieve host info via NetBIOS 
+```
+Format: IP GroupName\HostName ServiceType DeviceType
+    
+```
+![hostinfo](images/HostInfo.JPG)
---
+++ SharpNBTScan/SharpNBTScan/SharpNBTScan/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SharpNBTScan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SharpNBTScan")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a398655a-d83f-46bf-8173-3ad16260d970")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
